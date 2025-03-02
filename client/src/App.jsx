4faff9414b279ef2979ffd36cc1dc0467a3311b1
Anjali Ray <rayyyanjali@gmail.com>
2025-03-02 22:45:44 +0530

    Client Side Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+export default function App() {
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+   </BrowserRouter>
+  )
+}
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+      <h1>Hello User! This is Register Section</h1>
+    </div>
+  )
+}
+
+export default Register
